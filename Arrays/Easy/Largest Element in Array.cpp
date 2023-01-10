766d21b482b05330594bb7ebcf1d27f5bd7a87df
Shashank Singh <82604413+shashanksingh2002@users.noreply.github.com>
2023-01-10 20:45:23 +0530

    Update Largest Element in Array.cpp

--- Arrays/Easy/Largest Element in Array.cpp
+++ Arrays/Easy/Largest Element in Array.cpp
@@ -1,1 +1,13 @@
+class Solution
+{
+public:
+    int largest(vector<int> &arr, int n)
+    {
+        int maxi=INT_MIN;
+        for(int i=0;i<n;i++){
+            if(arr[i]>maxi) maxi=arr[i];
+        }
+        return maxi;
+    }
+};
 
