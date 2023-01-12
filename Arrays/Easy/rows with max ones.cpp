c0c1d1496aa51227101afe7996c89d8ba1a9c5a0
Shashank Singh <82604413+shashanksingh2002@users.noreply.github.com>
2023-01-12 13:04:58 +0530

    Create rows with max ones.cpp

---
+++ Arrays/Easy/rows with max ones.cpp
@@ -0,0 +1,29 @@
+class Solution{
+public:
+    int lower_bound(vector<vector<int>> &arr,int i,int m){
+        int st = 0,end = m-1;
+        while(st<=end){
+            int mid = st+(end-st)/2;
+            if(arr[i][mid]==1 && mid==0 || arr[i][mid]==1 && mid>0 && arr[i][mid-1]==0){
+                return mid;
+            }
+            else if(arr[i][mid]==1) end=mid-1;
+            else st = mid+1;
+        }
+        return -1;
+    }
+	int rowWithMax1s(vector<vector<int> > arr, int n, int m) {
+	    int maxi=0,idx=-1;
+	    for(int i=0;i<n;i++){
+	        int stIdx = lower_bound(arr,i,m);
+	        if(stIdx != -1){
+	            if(maxi<m-stIdx){
+	                maxi = m-stIdx;
+	                idx = i;
+	            }
+	        }
+	    }
+	    return idx;
+	}
+
+};
