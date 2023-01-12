64435e40412c1cb3e871be2e74a38e7120ccf40c
Shashank Singh <82604413+shashanksingh2002@users.noreply.github.com>
2023-01-12 23:11:22 +0530

    Update 2sumproblem.cpp

--- Arrays/Medium/2sumproblem.cpp
+++ Arrays/Medium/2sumproblem.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> umap;
+        vector<int> ans(2);
+        for(int i=0;i<nums.size();++i){
+            int search = target - nums[i];
+            if(umap.find(search) != umap.end()){
+                ans[0] = umap[search];
+                ans[1] = i;
+                break;
+            }
+            else{
+                umap[nums[i]] = i;
+            }
+        }
+        return ans;
+    }
+};
